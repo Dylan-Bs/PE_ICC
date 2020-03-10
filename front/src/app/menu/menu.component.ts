6e42e45da35008b23c27cd0d3e13456e3851ff90
Deakcor <deak24cor@gmail.com>
2020-03-10 14:18:15 +0100

    topbar mobile/windows

--- front/src/app/menu/menu.component.ts
+++ front/src/app/menu/menu.component.ts
@@ -9,8 +9,24 @@ import { AppComponent } from '../app.component';
 })
 export class MenuComponent implements OnInit {
   constructor(public conne:ConnexionService, public  parent:AppComponent) { }
+  isMobile = false;
+  getIsMobile(): boolean {
+    const w = document.documentElement.clientWidth;
+    const breakpoint = 992;
+    console.log(w);
+    if (w < breakpoint) {
+      return true;
+    } else {
+      return false;
+
+    }
+  }
 
   ngOnInit() {
+    this.isMobile = this.getIsMobile();
+    window.onresize = () => {
+      this.isMobile = this.getIsMobile();
+    };
   }
 
 }
