643fe528c1ec9913f8aab9659c04046392581c58
Deakcor <deak24cor@gmail.com>
2020-05-14 15:31:07 +0200

    mobile navbar update
    Fermer la navbar mobile quand on change de root et effectuer les mêmes changement que sur la barre classique

--- front/src/app/app.component.ts
+++ front/src/app/app.component.ts
@@ -1,23 +1,57 @@
-import { Component } from '@angular/core';
+import { Component, OnInit } from '@angular/core';
 import { FirebaseApp } from '@angular/fire';
 import { ConnexionService } from './services/connexion.service';
 import { MatSidenav } from '@angular/material/sidenav';
+import { Router, NavigationEnd } from '@angular/router';
 
 @Component({
   selector: 'app-root',
   templateUrl: './app.component.html',
-  styleUrls: ['./app.component.css']
+  styleUrls: ['./app.component.css'],
+  host: {
+    '(window:resize)': 'onResize($event)'
+  }
 })
-export class AppComponent {
+export class AppComponent implements OnInit {
   title:string = 'projet-angular-app';
 
   open:boolean=false;
-  constructor(public conne:ConnexionService,public app: FirebaseApp) {
+  constructor(public conne:ConnexionService,public app: FirebaseApp,public router:Router) {
+    router.events.subscribe((val) => {
+      if (val instanceof NavigationEnd){
+        this.close()
+      }
+  });
 
   }
 
   _toggle(){
     this.open=!this.open;
+  }
+
+  close(){
+    this.open=false;
+  }
+
+  isMobile = false;
+  getIsMobile(): boolean {
+    const w = document.documentElement.clientWidth;
+    const breakpoint = 992;
+    if (w < breakpoint) {
+      return true;
+    } else {
+      this.close()
+      return false;
+
+    }
+  }
+
+  onResize(event){
+    this.isMobile = this.getIsMobile();
+  }
+
+  ngOnInit() {
+    this.isMobile = this.getIsMobile();
   }
 
   
--- front/src/app/menu/menu.component.ts
+++ front/src/app/menu/menu.component.ts
@@ -1,19 +1,22 @@
 import { Component, OnInit } from '@angular/core';
 import { ConnexionService } from '../services/connexion.service';
 import { AppComponent } from '../app.component';
+import { Router } from '@angular/router';
 
 @Component({
   selector: 'app-menu',
   templateUrl: './menu.component.html',
-  styleUrls: ['./menu.component.css']
+  styleUrls: ['./menu.component.css'],
+  host: {
+    '(window:resize)': 'onResize($event)'
+  }
 })
 export class MenuComponent implements OnInit {
-  constructor(public conne:ConnexionService, public  parent:AppComponent) { }
+  constructor(public conne:ConnexionService, public  parent:AppComponent,public router:Router) { }
   isMobile = false;
   getIsMobile(): boolean {
     const w = document.documentElement.clientWidth;
     const breakpoint = 992;
-    console.log(w);
     if (w < breakpoint) {
       return true;
     } else {
@@ -22,11 +25,12 @@ export class MenuComponent implements OnInit {
     }
   }
 
+  onResize(event){
+    this.isMobile = this.getIsMobile();
+  }
+
   ngOnInit() {
     this.isMobile = this.getIsMobile();
-    window.onresize = () => {
-      this.isMobile = this.getIsMobile();
-    };
   }
 
 }
