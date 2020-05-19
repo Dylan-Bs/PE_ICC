39ff385816bae1a0665c40c27a8e4b22ba6e6bc2
fabienlrct <fabienlaurencet@gmail.com>
2020-05-19 16:26:13 +0200

    create superuser from script

---
+++ scripts/create-SuperUser.py
@@ -0,0 +1,11 @@
+from django.contrib.auth.models import User
+ 
+def run():
+    if not User.objects.filter(username='adminPEICC').count():
+        print("Creating superuser")
+        user = User.objects.create_user('adminPEICC', 'admin@peicc.com', 'adminpeic*')
+        user.first_name = 'Admin'
+        user.last_name = 'PeICC'
+        user.is_staff = True
+        user.is_superuser = True
+        user.save()
