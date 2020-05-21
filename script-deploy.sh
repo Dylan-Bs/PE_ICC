fca72e7c96d738adbc5909c27c261596638abf88
Dylan Bersans <35694107+Dylan-Bs@users.noreply.github.com>
2020-05-21 20:27:06 +0200

    Update script-deploy.sh
    change stop to down and add remove images

--- script-deploy.sh
+++ script-deploy.sh
@@ -3,7 +3,7 @@
 # Vérification si un docker-compose est déjà lancé
 if [ -f dockercompose.lock ]; then
   echo "Arret du précédant docker-compose"
-  docker-compose stop
+  docker-compose down --rmi all --volumes --remove-orphans
   rm dockercompose.lock
 fi
 echo "Lancement docker compose"
