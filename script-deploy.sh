306f8de26ed7456653c267f2e8852edae9fe606b
Dylan Bersans <35694107+Dylan-Bs@users.noreply.github.com>
2020-05-21 21:19:07 +0200

    fix script-deploy

--- script-deploy.sh
+++ script-deploy.sh
@@ -1,10 +1,10 @@
  #/bin/sh -x
 
 # Vérification si un docker-compose est déjà lancé
-if [ -f dockercompose.lock ]; then
+isUp=$(docker-compose ps front | grep Up)
+if [ -n "$isUp" ]; then
   echo "Arret du précédant docker-compose"
   docker-compose down --rmi all --volumes --remove-orphans
-  rm dockercompose.lock
 fi
 echo "Lancement docker compose"
 docker-compose up -d --build --remove-orphans
