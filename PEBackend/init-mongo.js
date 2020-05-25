494f5ee630c44318cbe577e561a34fa968e38c34
Dylan Bersans <dylan.bersans@eisti.eu>
2020-05-25 14:11:07 +0200

    fix merge back + front

--- PEBackend/init-mongo.js
+++ PEBackend/init-mongo.js
@@ -1,3 +1,2 @@
-db.users.insert(
-    {firstName:"Fabien",lastName:"Laurencet",companyName:"Exakis-Nelite", city:"Bidart", department:"Pyrénées-Atlantique",zip:"64210", phone1:"504-621-8927",email:"fabienlaurencet@gmail.com"}
-) 
+db.createCollection("teacher")
+db.createCollection("student")
