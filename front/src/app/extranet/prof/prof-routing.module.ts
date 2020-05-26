ab5f06acf3e32cde1e77d32c5963f0f376569654
Eliott Villegas <villegasel@eisti.eu>
2020-05-27 00:16:28 +0200

    edition info prof

--- front/src/app/extranet/prof/prof-routing.module.ts
+++ front/src/app/extranet/prof/prof-routing.module.ts
@@ -8,11 +8,13 @@ import { ViewDetailsComponent } from './view-details/view-details.component';
 import { ViewDetailsResolver } from './view-details/view-details.resolver';
 import { paths } from 'src/app/app-paths';
 import { StatsResolver } from './stats/stats.resolver';
+import { EditionProfComponent } from './edition-prof/edition-prof.component';
 
 
 const profRoutes: Routes = [
   { path: paths.empty, canActivate: [AuthGuard, ProfGuard], component: ListeEtudComponent },
   { path: paths.stats, canActivate: [AuthGuard, ProfGuard], component: StatsComponent,resolve: { data: StatsResolver } },
+  { path: paths.edition_pr, canActivate: [AuthGuard, ProfGuard],  component: EditionProfComponent },
   { path: paths.details_pr, canActivate: [AuthGuard, ProfGuard], component: ViewDetailsComponent, resolve: { data: ViewDetailsResolver } }
 ];
 
